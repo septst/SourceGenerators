a72105f5f9b7b185443be14b6844f8f45b09c809
septst <parthibansekar87@gmail.com>
2022-07-27 23:19:43 +0100

    Added HelloFrom generator

---
+++ SourceGeneratorConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SourceGeneratorConsole;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        HelloFrom("Generated Code");
+        Console.WriteLine("Hello from Console");
+        Console.ReadKey();
+    }
+
+    static partial void HelloFrom(string name);
+}
---
+++ SourceGeneratorLibrary/HelloSourceGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+
+namespace SourceGeneratorLibrary
+{
+    [Generator]
+    public class HelloSourceGenerator: ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+#if DEBUG
+            if (!Debugger.IsAttached)
+            {
+                Debugger.Launch();
+            }
+
+            SpinWait.SpinUntil(() => Debugger.IsAttached);
+#endif 
+            Debug.WriteLine("No initialization is required here");
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var mainMethod = context.Compilation.GetEntryPoint(context.CancellationToken);
+            var mainMethodNameSpace = mainMethod?.ContainingNamespace.ToDisplayString();
+            var typeName = mainMethod?.ContainingType.Name;
+            
+            Debug.WriteLine("Begin: Preparing Source");
+            var source = $@"// Auto-generated code
+                            using System;
+                            namespace {mainMethodNameSpace};
+                            
+                            public static partial class {typeName}
+{{
+    static partial void HelloFrom(string name) => 
+        Console.WriteLine($""Generator says: Hi from '{{name}}'"");
+}}";
+            Debug.WriteLine($"Source: {source}");
+
+            Debug.WriteLine("Finish: Preparing Source");
+
+            context.AddSource($"{typeName}.g.cs", source);
+            Debug.WriteLine("Source successfully added.");
+        }
+    }
+}
